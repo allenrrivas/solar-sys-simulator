3e5d6633aab75b95bdb13f9c54a10fc80050c66a
allenrrivas <allenrrivas.30@gmail.com>
2024-10-12 20:38:38 -0700

    added getters and setters for planet object

--- src/planet.h
+++ src/planet.h
@@ -4,9 +4,27 @@
 #include <iostream>
 #include <string>
 
-class Planet{
+class Planet
+{
 public:
+    // Constructor
+    Planet(std::string planet_name, double planet_mass, double planet_radius, double planet_distance_from_sun)
+        : name(planet_name), mass(planet_mass), radius(planet_radius), distance_from_sun(planet_distance_from_sun) {}
+
+    // Getters
+    std::string get_name() const { return name; }
+    double get_mass() const { return mass; }
+    double get_radius() const { return radius; }
+    double get_distance_from_sun() const { return distance_from_sun; }
+
+    // Setters
+    void set_name(std::string planet_name) { name = planet_name; }
+    void set_mass(double planet_mass) { mass = planet_mass; }
+    void set_radius(double planet_radius) { radius = planet_radius; }
+    void set_distance_from_sun(double planet_distance_from_sun) { distance_from_sun = planet_distance_from_sun; }
+
     void planet_details();
+
 private:
     std::string name;
     double mass;
